2057b7db8841ab92ed1ff38eba5dd5815e5f13b1
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2018-11-28 10:20:58 +0530

    introduced selectSide and selectAlive function in util

--- src/util.js
+++ src/util.js
@@ -1,3 +1,5 @@
+const readline = require('readline-sync').question;
+
 const repeatCharacter = function(symbol,width,delimeter){
   return new Array(width).fill(symbol).join(delimeter);
 }
@@ -6,4 +8,18 @@ const makeHorizontalBorder = function(width){
   return "+"+repeatCharacter("---+",width,"");
 }
 
-module.exports = {repeatCharacter, makeHorizontalBorder}
+const selectSide = function(){
+  let side = readline('Enter side of square: ');
+  return side;
+}
+
+const selectAlive = function(){
+  let moves = readline('Enter the position of live cells(use , as seperator): ');
+  return moves.split(',');
+};
+
+module.exports = {
+  repeatCharacter,
+  makeHorizontalBorder,
+  selectSide, 
+  selectAlive }
