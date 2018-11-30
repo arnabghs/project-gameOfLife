173a588a9ad651a2619f7729c39262a72a4fcfaf
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2018-12-01 00:34:30 +0530

    Modified lib
    modified getModifiedCurrGen & getModifiedNextGen function.
    Removed unnecessary functions.
    modified nextGeneration Function.
    replaced logSampleSpace to a more suitable place in the file.

--- src/lib.js
+++ src/lib.js
@@ -36,6 +36,12 @@ const outlineGenerator = function(length,width,object){
   return outline;
 }
 
+const logSampleSpace = function(length,width,aliveArray){
+  let emptyObject = createObject(length,width);
+  let presentObject = produceAlive(emptyObject,aliveArray);
+  console.log(outlineGenerator(length,width,presentObject));
+}
+
 
 const getAllNeighbours = function(length,width,position){
   if((position-1)%length == 0) return getNeighboursFirstColumn(length,width,position);
@@ -56,46 +62,28 @@ const produceNextGenAliveCells = function(length,width,object,aliveArray){
   return aliveCells.concat(deadCells.filter(x => aliveNeighbourLength(x) == 3));
 }
 
-const logSampleSpace = function(length,width,aliveArray){
-  let emptyObject = createObject(length,width);
-  let presentObject = produceAlive(emptyObject,aliveArray);
-  console.log(outlineGenerator(length,width,presentObject));
-}
-
-const filterCellsWithinBound = function(bounds){
-  let length = bounds.bottomRight[1]+1;
-  let width = bounds.bottomRight[0]+1;
-  let sampleArray = [];
-  for (let i= 1; i<= length*width; i++){
-    let value = convertValueToCoordinate([i],length);
-    if(value[0][0] >= bounds.topLeft[0] && value[0][1] >= bounds.topLeft[1]){
-      sampleArray.push(value[0]);
-    }
-  }
-  return sampleArray;
-}
-
-const selectAliveWithinBound = function(currGeneration,bounds){
-  let filteredSampleSpace = filterCellsWithinBound(bounds);
-  return currGeneration.filter(x => filteredSampleSpace.some(y => y.toString() === x.toString()));
-}
-
 const getModifiedCurrGen = function (currGeneration,bounds){
-  let diff = bounds.topLeft[0] - 0;
-  return currGeneration.map(x => x.map(y => y-diff));
+  return currGeneration.map(function(x){
+    x[0] = x[0] - bounds.topLeft[0];
+    x[1] = x[1] - bounds.topLeft[1];
+    return x;
+  })
 }
 
 const getModifiedNextGen = function (currGeneration,bounds){
-  let diff = bounds.topLeft[0] - 0;
-  return currGeneration.map(x => x.map(y => y+diff));
+  return currGeneration.map(function(x){
+    x[0] = x[0] + bounds.topLeft[0];
+    x[1] = x[1] + bounds.topLeft[1];
+    return x;
+  })
 }
 
+
 const nextGeneration = function(currGeneration,bounds) {
-  let liveCellsInBound = selectAliveWithinBound(currGeneration,bounds);
-  let modifiedCurrGen =  getModifiedCurrGen(liveCellsInBound,bounds);
+  let modifiedCurrGen =  getModifiedCurrGen(currGeneration,bounds);
   let {length,width,livePositionValue } = convertCoordinateToValue(modifiedCurrGen,bounds);
-  let inputValueArray = produceNextGenAliveCells(length,width,createObject(length,width),livePositionValue);
-  let nextGenCoordinates = convertValueToCoordinate(inputValueArray,length);
+  let nextGenValues = produceNextGenAliveCells(length,width,createObject(length,width),livePositionValue).sort((a,b) => a-b);
+  let nextGenCoordinates = convertValueToCoordinate(nextGenValues,length);
   return getModifiedNextGen(nextGenCoordinates,bounds);
 }
 
@@ -105,7 +93,6 @@ module.exports = {
   getLiveNeighboursLength,
   produceNextGenAliveCells,
   logSampleSpace,
-  selectAliveWithinBound,
   getModifiedCurrGen,
   getModifiedNextGen,
   nextGeneration }
