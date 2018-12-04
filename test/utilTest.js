d86746f759367856f9451f8139413ab48ad89532
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2018-12-04 08:18:27 +0530

    forgot to edit tests in previous commit

--- test/utilTest.js
+++ test/utilTest.js
@@ -22,10 +22,10 @@ describe ("Test for util",function(){
   });
   describe("test for makeHorizontalBorder",function(){
     it('with 0 width should only return the hardcoded +',function(){
-      assert(makeHorizontalBorder(0),"+");
+      assert(makeHorizontalBorder(0),"+-+");
     });
     it('with n width should return the unit string n times connected as a string',function(){
-      assert(makeHorizontalBorder(5),"+---+---+---+---+---+");
+      assert(makeHorizontalBorder(5),"+-----------+");
     });
   });
   describe("test for createObject",function(){
